863acf8fa4827b9b4f67a835db7325fae3ab1f9c
Devan <dng8597@bths.edu>
2023-11-14 18:41:59 -0500

    added stuff idk

---
+++ src/model/solvers/FloodFillFirstSearchSolver.java
@@ -0,0 +1,170 @@
+package model.solvers;
+
+import controller.TileUpdate;
+import controller.ViewUpdatePacket;
+import model.Cell;
+import model.Grid;
+import utilities.Constants;
+
+import java.util.*;
+
+public class FloodFillFirstSearchSolver extends Solver {
+
+    private final Comparator<Cell> hueristicComparator = new Comparator<Cell>() {
+        @Override
+        public int compare(Cell o1, Cell o2) {
+            return Integer.compare(heuristic(o1), heuristic(o2));
+        }
+    };
+    // The child is the key and the parent is the value
+    private final HashMap<Cell, Cell> parentCells = new HashMap<>(Constants.mazeLength * Constants.mazeLength);
+    private final Stack<Cell> stack = new Stack<>();
+
+    private final Queue<Cell> queue = new LinkedList<>();
+
+    private boolean startStepDone = false;
+    private Cell currentCell;
+    private Cell targetCell;
+
+    public FloodFillFirstSearchSolver(Grid grid) {
+        super(grid);
+    }
+
+    public FloodFillFirstSearchSolver(Grid grid, Cell startPoint, ArrayList<Cell> endPoints) {
+        super(grid, startPoint, endPoints);
+    }
+
+    public Cell getCurrentCell() {
+        return currentCell;
+    }
+
+    public boolean isStartStepDone() {
+        return startStepDone;
+    }
+
+    public void setStartStepDone(boolean startStepDone) {
+        this.startStepDone = startStepDone;
+    }
+
+    public void floodFillPuesdo(Cell cell) {
+        queue.add(cell);
+
+        while (!(queue.isEmpty())) {
+
+            Cell cell_current_head = queue.remove();
+            ArrayList<Cell> list = this.getGrid().getAdjacentCells(cell_current_head); //command to get
+            for (Cell cell1 : list)
+            {
+                cell1.setCellValue(cell_current_head.getCellValue()+ 1);
+            }
+            queue.addAll(list);
+        }
+    }
+
+    @Override
+    public ViewUpdatePacket makeViewUpdatePacket() {
+        ViewUpdatePacket updatePacket = new ViewUpdatePacket(new ArrayList<>(300));
+
+
+        for (int x = Constants.minCellIndex; x <= Constants.maxCellIndex; x++) {
+            for (int y = Constants.minCellIndex; y <= Constants.maxCellIndex; y++) {
+
+                Cell cell = this.getGrid().getCell(x, y);
+
+
+                TileUpdate tileUpdate = Cell.makeTileUpdateFromCell(cell, false, false);
+                updatePacket.addTileUpdate(tileUpdate);
+            }
+        }
+
+        // Add the current cell at the end, will override its earlier addition
+        if (currentCell != null) {
+            TileUpdate tileUpdate = Cell.makeTileUpdateFromCell(this.getCurrentCell(), true, false);
+            updatePacket.addTileUpdate(tileUpdate);
+        }
+
+        if (targetCell != null) {
+            TileUpdate tileUpdate = Cell.makeTileUpdateFromCell(this.targetCell, false, true);
+            updatePacket.addTileUpdate(tileUpdate);
+        }
+
+
+        return updatePacket;
+    }
+
+    //Used for testing
+    public Comparator<Cell> getHueristicComparator() {
+        return hueristicComparator;
+    }
+
+    public int manhattanDistance(Cell current, Cell target) {
+        int dx = Math.abs(current.getxPos() - target.getxPos());
+        int dy = Math.abs(current.getyPos() - target.getyPos());
+        int retVal = dx + dy;
+        return retVal;
+    }
+
+    public int heuristic(Cell cell) {
+        int minDistance = Integer.MAX_VALUE;
+        int calculatedDistance = 0;
+        for (Cell target : this.endPoints) {
+            calculatedDistance = manhattanDistance(cell, target);
+            minDistance = Math.min(minDistance, calculatedDistance);
+        }
+        return minDistance;
+    }
+
+    public ArrayList<Cell> generateOrderedStackAppendList(Cell currentCell) {
+        ArrayList<Cell> neighbors = getUntraversedReachableNeighbors(currentCell);
+        neighbors.sort(Collections.reverseOrder(hueristicComparator)); // We want the closest to be ontop of the stack
+        return neighbors;
+    }
+
+    public void iterate() {
+        if (this.isDone()) {
+        } else if (!startStepDone) {
+            this.currentCell = startPoint;
+            this.currentCell.setTraversed(true);
+            List<Cell> neighbors = generateOrderedStackAppendList(currentCell);
+            this.stack.addAll(neighbors);
+            targetCell = stack.pop();
+            this.setStartStepDone(true);
+        } else if (atDestination(currentCell)) {
+            this.targetCell = null;
+            this.setDone(true);
+        } else if (Grid.isTherePathBetweenCells(currentCell, targetCell)) {
+            parentCells.put(targetCell, currentCell);
+            currentCell = targetCell;
+            currentCell.setTraversed(true);
+            ArrayList<Cell> neighbors = generateOrderedStackAppendList(currentCell);
+            stack.addAll(neighbors);
+            targetCell = stack.pop();
+        } else {
+            currentCell = parentCells.get(currentCell);
+        }
+    }
+
+    public void finish() {
+        while (!this.isDone()) {
+            this.iterate();
+        }
+    }
+
+    /* Heuristic Depth-First Search Explained for Maze Solving
+     * Step 1.
+     * Create an empty stack. Mark starting cell as visited.
+     * Push the reachable neighbors of the starting cell to the stack ordered by the heuristic. (Lowest cost on top)
+     *
+     * Step 2.
+     * While current_cell is not one of our destination cells:
+     *   1. Pop a cell from the stack. This is our target cell.
+     *   2. If we can move to the target cell from the current cell:
+     *       a. Assign the parentage of the target cell as current cell
+     *       b. Make current cell the target cell
+     *       c. Mark (now) current cell as traversed
+     *       d. Push all un-traversed reachable neighbors of current cell to the stack according to the heuristic. (Lowest cost on top)
+     *   3. Else (we cannot move to target cell from current cell):
+     *       a. Make current cell equal to the parent of current cell
+     *
+     */
+}
